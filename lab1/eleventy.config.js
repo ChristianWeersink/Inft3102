fd5251a6d0f403d1b20a67ba223298687673b6a0
ChristianWeersink <christian.weersink@dcmail.ca>
2024-09-17 22:04:55 -0400

    fix

--- lab1/eleventy.config.js
+++ lab1/eleventy.config.js
@@ -9,6 +9,11 @@ module.exports = function(eleventyConfig) {
 	// Return your Object options:
 	eleventyConfig.addPassthroughCopy("./src/public");
 	eleventyConfig.addWatchTarget("./public/");
+
+	// eleventyConfig.addPassthroughCopy({"./node_modules/bootstrap/dist/css/bootstrap.mins.css": "/css/bootstreap.min.css",
+	// "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js": "/js/bootstreap.bundle.min.js"
+	// });
+
 	eleventyConfig.on('beforeBuild', () => {
 		console.log('Building the site...');
 	  });
