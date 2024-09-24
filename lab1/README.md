961de65cac137a4125b4f34631d86ddce69601e0
ChristianWeersink <christian.weersink@dcmail.ca>
2024-09-24 16:05:14 -0400

    update instructions

--- lab1/README.md
+++ lab1/README.md
@@ -5,6 +5,6 @@ Steps to view lab:
 1. Open terminal
 2. npm install
 3. npm run build
-4. serve dist
-5. ctrl + click the local link in terminal or copy+paste into browser
+4. open dist folder and open index.html
+5. go live using liver server
 6. Use the drop down or H1 link to access the lab 1 write up
--- lab1/eleventy.config.js
+++ lab1/eleventy.config.js
@@ -10,6 +10,12 @@ module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("./src/public");
 	eleventyConfig.addWatchTarget("./public/");
 
+	// Configure base url for development or production
+	let env = process.env.ELEVENTY_ENV || 'development';
+  	let baseUrl = env === 'development' ? "" : "/dist";
+	// base url will be placed before each url so links work in both dev and production
+ 	eleventyConfig.addGlobalData("baseUrl", baseUrl);
+
 	// eleventyConfig.addPassthroughCopy({"./node_modules/bootstrap/dist/css/bootstrap.mins.css": "/css/bootstreap.min.css",
 	// "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js": "/js/bootstreap.bundle.min.js"
 	// });
